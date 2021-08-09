b8f53cbcd17313035bee4590f3e57b4039d439ee
SLU <tchiudolan@gmail.com>
2021-08-08 21:53:43 -0300

    Deu tudo errado nessa porra lek

--- assets/editaProduto.js
+++ assets/editaProduto.js
@@ -14,8 +14,7 @@ function editaProduto(evento) {
             column.contentEditable = "true";
             column = column.previousSibling;
         }
-        const paragrafoColumn = column.querySelector("p");
-        paragrafoColumn.focus();
+        
         editavel = false;
 
     } else if(editavel === false) {
